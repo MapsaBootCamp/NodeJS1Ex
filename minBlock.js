a4b1dd7c06691152b57170204a4d2bf65ddbbdc1
azamSedarat <nahid.sedarat.j@gmail.com>
2022-09-08 15:25:01 +0430

    clean code done

--- minBlock.js
+++ minBlock.js
@@ -1,19 +1,23 @@
-let t = 3;         //number of test cases
-let n = [5, 10, 5]     //number of rows
-let m =[7, 10, 7]      //number of culomns
-let a =[1, 2, 2]       //row length for blocks
-let b =[1, 3, 1]       //culomns length for blocks
+let testCases = 3;         //number of test cases
+let pageRows = [5, 10, 5]     //number of rows
+let pageCulomns =[7, 10, 7]      //number of culomns
+let blockRows =[1, 2, 2]       //row length for blocks
+let blockCulomns =[1, 3, 1]       //culomns length for blocks
 
-for(let i = 0 ; i < t ; i++){
-    minBlock(n[i], m[i], a[i], b[i])
+for(let i = 0 ; i < testCases ; i++){
+    minBlock(pageRows[i], pageCulomns[i], blockRows[i], blockCulomns[i])
 }
 
-function minBlock(n, m, a, b){
-    //maximum distance between 2 blocks is a-1 rows && b-1 culomns
-    //minimum number of blocks in row is a + (a - 1) 
-    let minBlocksInRow = n % (a + (a - 1)) < a ? Math.floor(n / (a + (a - 1))) : Math.floor(n / (a + (a - 1))) + 1
-    //minimum number of blocks in culomn is  b + (b - 1) 
-    let minBlocksInCulomn = m % (b + (b - 1)) < b ? Math.floor(m / (b + (b - 1))) : Math.floor(m / (b + (b - 1))) + 1
+function minBlock(pageRows, pageculomns, blockRows, blockCulomns){
+    //maximum distance between 2 blocks is blockRows - 1) rows && blockCulomns - 1 culomns
+    //minimum number of blocks in row is blockRows + (blockRows - 1)
+    let minBlocksInRow = pageRows % (blockRows + (blockRows - 1)) < blockRows ?
+    Math.floor(pageRows / (blockRows + (blockRows - 1))) :
+    Math.floor(pageRows / (blockRows + (blockRows - 1))) + 1
+    //minimum number of blocks in culomn is  blockCulomns + (blockCulomns - 1)
+    let minBlocksInCulomn = pageculomns % (blockCulomns + (blockCulomns - 1)) < blockCulomns ?
+    Math.floor(pageculomns / (blockCulomns + (blockCulomns - 1))) : 
+    Math.floor(pageculomns / (blockCulomns + (blockCulomns - 1))) + 1
     //minimum blocks is minBlocksInRow * minBlocksInCulomn
     console.log(minBlocksInRow * minBlocksInCulomn);
 }
