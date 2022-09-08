23d78aa2dd092fe6da1f9e47648093edb88ba7a0
azamSedarat <nahid.sedarat.j@gmail.com>
2022-09-08 11:37:00 +0430

    clean code done

--- NQueen.js
+++ NQueen.js
@@ -1,10 +1,10 @@
 Nqueen(5)
 function Nqueen(n){
-	const board=Array(n)
-	for(let i=0;i<n;i++){
-		board[i]=Array(n).fill(0);
+	const board = Array(n)
+	for(let i = 0 ; i < n ; i++){
+		board[i] = Array(n).fill(0);
 	}
-	if(solve(board,n, 0)){
+	if(solve(board, n, 0)){
 		console.log(board);
 		return;
 	}
@@ -12,28 +12,28 @@ function Nqueen(n){
 	return;
 }
 function solve(board, n, col){
-	for(let i=0;i<n;i++){
-		if(safety(board,n,i,col)){
-			board[i][col]=1
-			solve(board,n,col+1)
+	for(let i = 0 ; i < n ; i++){
+		if(safety(board, n, i, col)){
+			board[i][col] = 1
+			solve(board, n, col + 1)
 		}
 	}
 	return true;
 }
-function safety(board,n,row,col){
-	for(let i=col;i<n;i++){
+function safety(board, n, row, col){
+	for(let i = col ; i < n ; i++){
 		if(board[i][col])
 			return false;
 	}
-	for(let i=0;i<n;i++){
+	for(let i = 0 ; i < n ; i++){
 		if(board[row][i])
 			return false;
 	}
-	for(let i=row, j=col;i>=0 && j>=0;i--,j--){
+	for(let i = row, j = col ; i >= 0 && j >= 0 ; i-- , j--){
 		if(board[i][j])
 			return false;
 	}
-	for (i=row, j=col; j>=0 && i<n; i++, j--){
+	for (i = row , j = col ; j >= 0 && i < n ; i++ , j--){
 		if(board[i][j]) 
 			return false;
 	}
