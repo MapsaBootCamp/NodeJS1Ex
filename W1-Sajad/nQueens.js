9b0e492ec5d6f7b22dfcaf1d2bdce264656fb599
s4j4d <mmmsms97@gmail.com>
2022-09-11 16:39:24 +0430

    completed nQueens problem

--- W1-Sajad/nQueens.js
+++ W1-Sajad/nQueens.js
@@ -1,22 +1,62 @@
 
 
 
-function nQueen(numberOfQueens,visitedFirstTiles = null,path = []) {
-    
+function nQueen(numberOfQueens,visitedFirstTiles = null,unusableTiles = null,path = []) {
+    let nextTile;
     if(visitedFirstTiles == null){
-        visitedFirstTiles = Array(numberOfQueens).fill(Array(numberOfQueens).fill(0))
+        visitedFirstTiles = Array(numberOfQueens).fill([])
+        unusableTiles = Array(numberOfQueens).fill([])
+
+        for(let i = 0;i<numberOfQueens;i++){
+            visitedFirstTiles[i] = Array(numberOfQueens).fill(0)
+            unusableTiles[i] = Array(numberOfQueens).fill(0)
+
+        }
+        
     }
-    if()
-    for(let i = 0;i<visitedFirstTiles.length;i++ ){
-        for(let j =0;j<visitedFirstTiles.length;j++){
-            nQueen(numberOfQueens,visitedFirstTiles,path)
+    if(path.length == 0) {
+        nextTile = nextTileChooser(visitedFirstTiles)
+        if(!nextTile) return 'not possible'
+        visitedFirstTiles[nextTile[0]][nextTile[1]] = 1}
+    else{ nextTile = nextTileChooser(unusableTiles)
+        if(!nextTile){
+            if(path.length == numberOfQueens) return path
+            path = []
+            unusableTiles = Array(numberOfQueens).fill([])
+            for(let i = 0;i<numberOfQueens;i++){
+                unusableTiles[i] = Array(numberOfQueens).fill(0)
+            }
+            return nQueen(numberOfQueens,visitedFirstTiles,unusableTiles,path)
+        } 
+    }
+    path.push(nextTile)
+    unusableTiles =  tileRemover(nextTile,unusableTiles)
+    const result = nQueen(numberOfQueens,visitedFirstTiles,unusableTiles,path)
+    return result 
+}
+
+
+function tileRemover(nextTile,unusableTiles) {
+    for(let i=0;i<unusableTiles.length;i++){
+        for(let j=0;j<unusableTiles.length;j++){
+            if(i == nextTile[0]) unusableTiles[i][j] = 1
+            if(j == nextTile[1]) unusableTiles[i][j] = 1
+            if(i-j == nextTile[0]-nextTile[1]) unusableTiles[i][j] = 1
         }
     }
-
-
+    return unusableTiles
+}
+function nextTileChooser(arr){
+    const result = []
+    for(let i = 0;i<arr.length;i++){
+        for(let j = 0;j<arr.length;j++){
+            if(!arr[i][j]) return  [i,j]
+        }
+    }
+    return false
 }
 
 
-function collideCheck() {
-    
-}
+
+
+console.log(nQueen(4));
