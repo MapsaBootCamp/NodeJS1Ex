b5d3ff5395c877d3d8aa8cfcdcc65dca19f828a5
AbolfazlHosseini200 <78736666+AbolfazlHosseini200@users.noreply.github.com>
2022-09-19 09:35:18 +0430

    Add files via upload

---
+++ AbolfazlHosseini/MediaPlayer/mediaPlayer.ts
@@ -0,0 +1,36 @@
+import prompt from "prompt-sync"
+const scanner = prompt()
+
+abstract class Player{
+protected address:string=""
+abstract operation():void
+}
+class PathPlayer extends Player{
+constructor(ad:string){
+super()
+this.address = ad
+}
+operation(): void {
+    console.log(this.address," ","played")
+}
+}
+class UrlPlayer extends Player{
+constructor(ad:string){
+    super()
+    this.address = ad
+}
+operation(): void {
+    console.log(this.address," ","played")   
+}
+}
+function play(media:Player) {
+    media.operation()
+}
+while(true){
+const input:string = scanner("Enter Path(exit to end program): ")
+if(input === "exit") break;
+const re = new RegExp("^(http|https)://", "i");
+if(re.test(input))play(new UrlPlayer(input));
+else play(new PathPlayer(input));
+}
+console.log("bye!!!")
